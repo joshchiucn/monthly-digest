3906efbe903dc3c2015be9ed807a5e9dbe8f23a0
zhaoyouxin <382920428@qq.com>
2021-01-17 00:04:04 +0800

    feat: add some components

---
+++ src/components/page-header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Tab from './tab'
+function PageHeader () {
+  return (
+    <div className="page-header">
+      <Tab/>
+    </div>
+  )
+}
+
+export default PageHeader
